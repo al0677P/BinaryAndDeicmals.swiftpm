afc749b480392ddca90d0ff77ed8bf0fa4c7e8a2
Andres Lopez <alopez4578@stu.d214.org>
2023-09-20 10:24:28 -0500

    k

--- ContentView.swift
+++ ContentView.swift
@@ -1,12 +1,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var intnumbinary = ""
     var body: some View {
-        HStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView{
+            VStack {
+                
+                Button (action: {
+                    print( "Hello World tapped!")})
+                {
+                    Text ("Press me" )
+                    
+                }
+                        .background(Color.purple)
+                        .foregroundColor(.white)
+                        .font(.title)
+                        .cornerRadius(40)
+                
+                
+                TextField("Enter a number here", text: $intnumbinary)
+                    .navigationTitle("Decimals to Binary")
+                    .textFieldStyle(.roundedBorder)
+                    .frame(width: 200)
+                    .offset(y:-300)
+            }
+            
+           
+                
+                Button (action: {
+                    print( "Hello World tapped!")})
+                {Text ("Hello World" )}
+                    .background(.purple)
+                
+                
+                
+            }
         }
     }
-}
+
